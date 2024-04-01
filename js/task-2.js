495173c6f45a8477336467e3d699d694491af434
Neoriya <100101631+Neoriya@users.noreply.github.com>
2024-04-01 04:21:54 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,26 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const indexItemToRemove = this.#items.indexOf(itemToRemove);
+    if (indexItemToRemove !== -1) {
+      this.#items.splice(indexItemToRemove, 1);
+    }
+    return this.#items;
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
